fc001413d109d8329be680a3a316cc746c9342df
AlyaAlnuaimi <aliaalnuaimi111@gmail.com>
2022-12-17 15:21:35 +0400

    angilar session

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -8,7 +8,6 @@ import { Category } from '../interfaces/category';
 })
 export class CategoriesComponent {
   //we bring category[] from the interface
-  categories:Category[] = [
     {categoryName:'Category 1', imageUrl:'/assets/img/cat-1.jpg', productCount: 120},
     {categoryName:'Category 2', imageUrl:'/assets/img/cat-2.jpg', productCount: 30},
     {categoryName:'Category 3', imageUrl:'/assets/img/cat-3.jpg', productCount: 29},
