0f90eb58e032d4bee6e25e86e41aad3383c5871e
AlyaAlnuaimi <aliaalnuaimi111@gmail.com>
2022-12-19 23:35:28 +0400

    angular session

--- src/app/components/layouts/shopdetails/shopdetails.component.ts
+++ src/app/components/layouts/shopdetails/shopdetails.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 declare var $:any;
 
 @Component({
@@ -6,31 +6,42 @@ declare var $:any;
   templateUrl: './shopdetails.component.html',
   styleUrls: ['./shopdetails.component.css']
 })
-export class ShopdetailsComponent implements AfterViewInit {
+// after HTML rendering display (you may also like) -> afterViewInit
+export class ShopdetailsComponent implements OnInit, AfterViewInit, OnDestroy {
+  timer:any;
 
   ngAfterViewInit(): void {
-        // Related carousel
-        $('.related-carousel').owlCarousel({
-          loop: true,
-          margin: 29,
-          nav: false,
-          autoplay: true,
-          smartSpeed: 1000,
-          responsive: {
-              0:{
-                  items:1
-              },
-              576:{
-                  items:2
-              },
-              768:{
-                  items:3
-              },
-              992:{
-                  items:4
-              }
+    // Related carousel
+    $('.related-carousel').owlCarousel({
+      loop: true,
+      margin: 29,
+      nav: false,
+      autoplay: true,
+      smartSpeed: 1000,
+      responsive: {
+          0:{
+              items:1
+          },
+          576:{
+              items:2
+          },
+          768:{
+              items:3
+          },
+          992:{
+              items:4
           }
-      });
+      }
+  });
   }
 
+  ngOnInit(): void {
+    this.timer = setInterval(() => {
+      console.log(new Date())
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+     clearInterval(this.timer);
+  }
 }
